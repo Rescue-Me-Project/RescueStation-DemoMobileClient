8ac14674e176b0843c05943fc4abda0a77212c2e
Dave Mee <davemee@gmail.com>
2018-09-05 16:29:44 +0100

    Stupid minor "air quotes" nug

--- code/investigations/nativePushTest/www/js/index.js
+++ code/investigations/nativePushTest/www/js/index.js
@@ -28,7 +28,7 @@ var app = {
   // 'pause', 'resume', etc.
   onDeviceReady: function() {
     this.receivedEvent('deviceready');
-    initialisePush();
+    this.initialisePush();
   },
 
   // Update DOM on a Received Event
