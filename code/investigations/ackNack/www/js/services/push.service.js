41e67267e0e065950aaaeb994691202eb9366b38
Dave Mee <davemee@gmail.com>
2018-09-13 16:17:23 +0100

    🦖

--- code/investigations/ackNack/www/js/services/push.service.js
+++ code/investigations/ackNack/www/js/services/push.service.js
@@ -1,62 +1,63 @@
 (function() {
   'use strict';
-  console.log("pushSrvc executed");
+  $ionicPlatform.ready(function() {
+    console.log("pushSrvc executed");
 
-  angular
-    .module('push', [ 'ngCordova' ] )
-    .factory('pushSrvc', pushSrvc)
-  ;
+    angular
+      .module('push', [ 'ngCordova' ] )
+      .factory('pushSrvc', pushSrvc)
+    ;
 
-  pushSrvc.$inject = [
-    '$http',
-    '$cordovaPushV5'
-  ];
-  function pushSrvc(
-    $http,
-    $cordovaPushV5
-  ) {
-    var service = {};
+    pushSrvc.$inject = [
+      '$http',
+      '$cordovaPushV5'
+    ];
+    function pushSrvc(
+      $http,
+      $cordovaPushV5
+    ) {
+      var service = {};
 
-    service.push = undefined;
-    service.registrationId = undefined;
-    service.callbackHandler = undefined;
-    service.cordovaReady = false;
+      service.push = undefined;
+      service.registrationId = undefined;
+      service.callbackHandler = undefined;
+      service.cordovaReady = false;
 
-    service.options = {};
+      service.options = {};
 
-    document.addEventListener("deviceready", function () {
-      service.cordovaReady = true;
-    }, false);
+      document.addEventListener("deviceready", function () {
+        service.cordovaReady = true;
+      }, false);
 
-    service.initialisePush = function initialisePush( registeredCallback, messageCallback ) {
-      $cordovaPushV5.initialize(service.options).then(function(){
+      service.initialisePush = function initialisePush( registeredCallback, messageCallback ) {
+        $cordovaPushV5.initialize(service.options).then(function(){
 
-        $cordovaPushV5.onNotification();
-        $cordovaPushV5.onError();
+          $cordovaPushV5.onNotification();
+          $cordovaPushV5.onError();
 
-        $cordovaPushV5.register().then(
-          function registeredOkay( registrationId ) {
-            service.registrationId = registrationId;
-            if( messageCallback ) {
-              service.callbackHandler = messageCallback;
+          $cordovaPushV5.register().then(
+            function registeredOkay( registrationId ) {
+              service.registrationId = registrationId;
+              if( messageCallback ) {
+                service.callbackHandler = messageCallback;
+              }
+              if(registeredCallback) {
+                registeredCallback( registrationId );
+              }
             }
-            if(registeredCallback) {
-              registeredCallback( registrationId );
-            }
+          );
+        });
+
+        $rootScope.on('$cordovaPushV5:notificationReceived', function pushGotNotificationHandler(event, data) {
+          // data.[message|title|count|sound|image|additionalData]
+          console.log( "pushv5 got inbound message: ", event, data);
+          if(service.callbackHandler) {
+            service.callbackHandler( data );
           }
-        );
-      });
+        });
+      };
 
-      $rootScope.on('$cordovaPushV5:notificationReceived', function pushGotNotificationHandler(event, data) {
-        // data.[message|title|count|sound|image|additionalData]
-        console.log( "pushv5 got inbound message: ", event, data);
-        if(service.callbackHandler) {
-          service.callbackHandler( data );
-        }
-      });
-    };
-
-    return service;
+      return service;
+    }
   }
-
 })();
