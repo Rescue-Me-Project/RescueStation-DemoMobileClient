e59f4855821d5405b316c348d27b3f5122a7cfcf
Dave Mee <davemee@gmail.com>
2018-09-13 15:17:54 +0100

    Created new ackNack2 project (based on ackNack)

---
+++ code/investigations/ackNack2/www/js/modules/main/main.module.js
@@ -0,0 +1,20 @@
+console.log("main.module.js invoked");
+(function() {
+  'use strict';
+
+  angular
+    .module('main', [
+      //      'ionic'
+      'push'
+    ])
+    .config(function($stateProvider){
+      $stateProvider
+        .state('main', {
+          url: '/main',
+          xyztemplate: '<h1>HELLO</h1>',
+          templateUrl: 'js/modules/main/main.html',
+          controller: 'mainCtrl as vm'
+        });
+    });
+  console.log("and configured.");
+})();
