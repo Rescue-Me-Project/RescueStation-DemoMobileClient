7cdfc1b1e45eed05e0f6a477a9063f0c47cc2980
Dave Mee <davemee@gmail.com>
2018-09-13 14:44:06 +0100

    🐓

--- code/investigations/ackNack/www/js/app.js
+++ code/investigations/ackNack/www/js/app.js
@@ -28,6 +28,11 @@ angular.module('starter', [
       if (window.StatusBar) {
         StatusBar.styleDefault();
       }
+      console.log("window:", window);
+      console.log("_window:", _window);
+      console.log("window():", window());
+      console.log("_window():", _window());
+
       // ADDED START
       $state.go("main");
       // ADDED END
