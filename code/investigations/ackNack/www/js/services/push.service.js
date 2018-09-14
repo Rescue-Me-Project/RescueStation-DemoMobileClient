0f5d68126ac3ead3b51fa376473b32d52dbfab32
Dave Mee <davemee@gmail.com>
2018-09-14 09:58:53 +0100

    🦑

--- code/investigations/ackNack/www/js/services/push.service.js
+++ code/investigations/ackNack/www/js/services/push.service.js
@@ -5,9 +5,9 @@
 
   document.addEventListener("deviceready", function () {
 
-    $rootScope.$on('ionic-cordova-push-ready', function(){
-      console.log("all ready");
-    });
+//    $rootScope.$on('ionic-cordova-push-ready', function(){
+//      console.log("all ready");
+//    });
 
     angular
       .module('push', [ 'ngCordova' ] )
@@ -25,24 +25,24 @@
       $cordovaPushV5
     ) {
       var service = {};
-      
+
       service.push = undefined;
       service.registrationId = undefined;
       service.callbackHandler = undefined;
       service.cordovaReady = false;
-      
+
       service.options = {};
-      
+
       //    document.addEventListener("deviceready", function () {
       //      service.cordovaReady = true;
       //  }, false);
-      
+
       service.initialisePush = function initialisePush( registeredCallback, messageCallback ) {
         $cordovaPushV5.initialize(service.options).then(function(){
-          
+
           $cordovaPushV5.onNotification();
           $cordovaPushV5.onError();
-          
+
           $cordovaPushV5.register().then(
             function registeredOkay( registrationId ) {
               service.registrationId = registrationId;
@@ -55,7 +55,7 @@
             }
           );
         });
-        
+
         $rootScope.on('$cordovaPushV5:notificationReceived', function pushGotNotificationHandler(event, data) {
           // data.[message|title|count|sound|image|additionalData]
           console.log( "pushv5 got inbound message: ", event, data);
@@ -64,7 +64,7 @@
           }
         });
       };
-      
+
       return service;
     }
   }, false);
