f3521ff90d916c55ba0ad11f65533ed99ff75ac8
Dave Mee <davemee@gmail.com>
2018-09-20 14:11:34 +0100

    🍭

--- code/investigations/ackNack/www/js/services/push.service.js
+++ code/investigations/ackNack/www/js/services/push.service.js
@@ -96,18 +96,18 @@ window.FCMKEY = "AAAA2MBUecI:APA91bG4FOVHW4VDmlWud27Xh6hK5bGxcdfIl1cfGRETw-M24El
     // subscription handling
 
     service.subscribe = function subscribe( topic ) {
-      push.startSubscription( topic, function subscribeSuccess(){
+      service.push.startSubscription( topic, function subscribeSuccess(){
         console.log("push.service - subscription to '"+topic+"' successful!");
 
-        push.on('registration', function (data) {
+        service.push.on('registration', function (data) {
           alert('registrationId:' + data.registrationId);
         });
 
-        push.on('notification', function (data) {
+        service.push.on('notification', function (data) {
           alert('push:' + JSON.stringify(data));
         });
 
-        push.on('error', function (e) {
+        service.push.on('error', function (e) {
           alert('error: ' + e.message);
         });
 
