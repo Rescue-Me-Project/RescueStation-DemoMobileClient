a61b6e704506c064226f4118247f75800323ff7e
Dave Mee <davemee@gmail.com>
2018-10-08 11:25:29 +0100

    sackNack is a full copy of ackNack ...
    ... except www directory from serverAckNack, so there's a trivial chance it will build

---
+++ code/investigations/sackNack/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,28 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "phonegap-plugin-push.PushNotification",
+    "file": "plugins/phonegap-plugin-push/www/push.js",
+    "pluginId": "phonegap-plugin-push",
+    "clobbers": [
+      "PushNotification"
+    ]
+  },
+  {
+    "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+    "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+    "pluginId": "phonegap-plugin-barcodescanner",
+    "clobbers": [
+      "cordova.plugins.barcodeScanner"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "phonegap-plugin-push": "2.1.2",
+  "phonegap-plugin-barcodescanner": "8.0.0"
+};
+// BOTTOM OF METADATA
+});
