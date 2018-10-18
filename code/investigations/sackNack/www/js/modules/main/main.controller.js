b7f356ed73f42bee1a6794060f52d6397de1c0b4
Dave Mee <davemee@gmail.com>
2018-10-18 14:28:31 +0100

    gah

--- code/investigations/sackNack/www/js/modules/main/main.controller.js
+++ code/investigations/sackNack/www/js/modules/main/main.controller.js
@@ -258,7 +258,7 @@
         message_id: uuid.v4(),
         message_type: vm.MESSAGE_TYPE_ID.MESSAGE,
         sender_role: vm.role,
-        payload: { "message" : "hello" } ,
+        payload: '{ "message" : "hello" }' ,
         payload_format_type: vm.MESSAGE_PAYLOAD_TYPE_ID.INTEGER
       };
       pushSrvc.sendPayload( responsePayload ).then( function sendPayloadOkay(indata) {
