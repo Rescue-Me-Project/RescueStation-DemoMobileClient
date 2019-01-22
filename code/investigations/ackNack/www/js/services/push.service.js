fa0cc71f068a7c510f722479c7e621c110b9c2a8
Matrix374 <33572077+Matrix374@users.noreply.github.com>
2019-01-22 13:35:19 +0000

    Revert "BLSAH"
    This reverts commit d66a134cedea343a0819d4aacf71909897eb9751.

--- code/investigations/ackNack/www/js/services/push.service.js
+++ code/investigations/ackNack/www/js/services/push.service.js
@@ -1,4 +1,4 @@
-window.FCMKEY = "AAAAenDG18k:APA91bGOJNxYIaJ2DztcscLrk8tIUmxkAWSxd8jjXBt5gR19Wi33mKDlMeTQCWIEswptftT6ANv8P81_TSuoUxn8zXMuQn9IGVt43etpXbXh4VVHNcLNhaXZ483YqFxAmXOjXS5rhiuV";
+window.FCMKEY = "ENTER_YOUR_OWN_KEY_HERE";
 
 (function() {
   'use strict';
